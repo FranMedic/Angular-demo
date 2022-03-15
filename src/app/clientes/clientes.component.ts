62c6e268fdacf1f06325e30632c0b3c43cd3978f
Francisca Medico Moris <fmedico@carver-as.com>
2022-03-15 13:39:49 +0100

    added list and and cards with data from json

---
+++ src/app/clientes/clientes.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from "@angular/core";
+import { Cliente } from "./cliente";
+import { CLIENTES } from "./clientes.json";
+
+@Component({
+  selector: "app-clientes",
+  templateUrl: "./clientes.component.html",
+  styleUrls: ["./clientes.component.css"],
+})
+export class ClientesComponent implements OnInit {
+  clientes: Cliente[];
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.clientes = CLIENTES;
+  }
+}
---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,26 @@
+import { Cliente } from "./cliente";
+
+export const CLIENTES: Cliente[] = [
+  {
+    id: 1,
+    name: "Kermit",
+    surname: "The Frog",
+    createAt: "15/03/1997",
+    photo: "kermit2.png",
+  },
+  {
+    id: 2,
+    name: "Peggy",
+    surname: "The Pig",
+    createAt: "14/03/1997",
+    photo:
+      "https://i.pinimg.com/474x/c5/87/fb/c587fbb42aeb07e5ff1a1308d9c32b59--piggies-the-muppets.jpg",
+  },
+  {
+    id: 3,
+    name: "Cookie",
+    surname: "Monster",
+    createAt: "13/03/1997",
+    photo: "https://i.scdn.co/image/ab6761610000e5eba3a7cba23d68a4e73c3b8155",
+  },
+];
