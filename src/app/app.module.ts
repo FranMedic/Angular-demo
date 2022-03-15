a1544b1aafe84ff04221bb8507f84e629c487b3b
Franny Medico <m.francisca.medico@gmail.com>
2022-03-15 19:24:49 +0100

    added router and observables

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,15 @@ import { BrowserModule } from "@angular/platform-browser";
 import { AppComponent } from "./app.component";
 import { FooterComponent } from "./footer/footer.component";
 import { HeaderComponent } from "./header/header.component";
-
 import { ClientesComponent } from "./clientes/clientes.component";
+import { RouterModule, Routes } from "@angular/router";
+import { DirectivaComponent } from "./directiva/directiva.component";
 
+const routes: Routes = [
+  { path: "", redirectTo: "/clientes", pathMatch: "full" }, //home
+  { path: "clientes", component: ClientesComponent }, //path que queremos
+  { path: "directivas", component: DirectivaComponent },
+];
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,7 +20,7 @@ import { ClientesComponent } from "./clientes/clientes.component";
     FooterComponent,
     ClientesComponent,
   ],
-  imports: [BrowserModule],
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from "@angular/core";
 import { Cliente } from "./cliente";
 import { CLIENTES } from "./clientes.json";
+import { Observable, of } from "rxjs";
+
 @Injectable({
   providedIn: "root",
 })
 export class ClienteService {
   constructor() {}
-  getClientes(): Cliente[] {
-    return CLIENTES;
+  getClientes(): Observable<Cliente[]> {
+    return of(CLIENTES);
   }
 }
