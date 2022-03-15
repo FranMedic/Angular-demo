705199fdcf55dbac906b931a2fbb5c94f0abe84c
Francisca Medico Moris <fmedico@carver-as.com>
2022-03-15 11:06:45 +0100

    added component header and basic footer

---
+++ src/app/footer/footer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-footer",
+  templateUrl: "./footer.component.html",
+  styleUrls: ["./footer.component.css"],
+})
+export class FooterComponent {
+  telf: string = "923 646 882";
+  Localitation: string = "Calle París 184 4º";
+}
